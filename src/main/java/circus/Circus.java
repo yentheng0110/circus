bf4e1179a0571ec14f6999ae7578f564a30efd6e
okkhoy <okkhoy@gmail.com>
2024-09-06 17:44:44 +0800

    Refactor Circus and Trainer classes

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -1,3 +1,5 @@
+package circus;
+
 import circus.animal.Animal;
 import circus.animal.Duck;
 import circus.animal.Parrot;
@@ -23,25 +25,22 @@ public class Circus {
         }
     }
 
-    private static int calculateValue(Equipment[] equipments) {
+    private static int calculateAssetValue(Asset[] assets) {
         int total = 0;
-        for (Equipment e : equipments) {
-            if (e.getValue() <= 5) {
-                System.out.println("Ignoring low value item: " + e.getValue());
-            } else {
-                total += e.getValue();
-                System.out.println("Adding item value: " + e.getValue());
-                // some 
-                // more
-                // code 
-                // here ...
+        for (Asset a : assets) {
+            if (a.getValue() <= 5) {
+                System.out.println("Ignoring low value item: " + a.getValue());
+                continue;
             }
+            total += a.getValue();
+            System.out.println("Adding item value: " + a.getValue());
         }
         return total;
     }
 
     public static void main(String[] args) {
         makeAnimalsTalk();
-        System.out.println("Total value of equipments " + calculateValue(equipments));
+        System.out.println("Total value of animals " + calculateAssetValue(animals));
+        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
     }
 }
--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -1,6 +1,9 @@
+package circus;
+
 import circus.animal.Animal;
 import circus.animal.Bird;
 import circus.animal.Duck;
+import circus.animal.Parrot;
 
 public class Trainer {
     public static void main(String[] args) {
@@ -17,11 +20,7 @@ public class Trainer {
         getToSpeak(d2);
 
         train(new Duck());
-        // train(new circus.animal.Parrot());
-
-        Animal a2 = new Animal();
-        Bird b2 = new Bird();
-
+        // train(new animal.Parrot());
     }
 
     private static void getToSpeak(Animal animal) {
@@ -29,7 +28,9 @@ public class Trainer {
     }
 
     private static void train(Bird bird) {
-        Duck d = (Duck) bird;
-        d.swim();
+        if (bird instanceof Duck) {
+            Duck d = (Duck) bird;
+            d.swim();
+        }
     }
 }
