20f7c92e0bca6cf9ab75e9419bb454bb86a9c09c
e0968962@u.nus.edu <e0968962@u.nus.edu>
2024-09-13 17:20:14 +0800

    Add method to print the number of animals

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -50,5 +50,8 @@ public class Circus {
         makeAnimalsTalk();
         System.out.println("Total value of animals " + calculateAssetValue(animals));
         System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+    private static void printNumberOfAnimalsInTheCircus(String animalArrayList) {
+        System.out.println(animalArrayList);
+    }
     }
 }
